97a31c3901e4348078a089c2bc5603057ea963cf
RAJ <rajkamal11126@gmail.com>
2020-08-21 01:27:10 +0530

    added atleast 1 uppercase

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -2,9 +2,8 @@
 shopt -s extglob
 echo "Enter your password :"
 read passwd
-
-pat1='[[:upper:]]{1}'
-pat2='[a-zA-Z0-9]{7,}$'
+pat1='^(.){8,}'
+pat2='[[:upper:]]{1}'
 if [[ $passwd =~ $pat ]]
 then
         echo "valid"

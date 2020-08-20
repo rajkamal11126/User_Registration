8566cf6a4cc5feb5ee0f3ae76a8b101b5ff81d21
RAJ <rajkamal11126@gmail.com>
2020-08-21 01:31:57 +0530

    added atleast 1 uppercase

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -4,7 +4,7 @@ echo "Enter your password :"
 read passwd
 pat1='^(.){8,}'
 pat2='[[:upper:]]{1}'
-if [[ $passwd =~ $pat ]]
+if [[ $passwd =~ $pat1 && $passwd =~ $pat2 ]]
 then
         echo "valid"
 else

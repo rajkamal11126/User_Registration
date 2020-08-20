eb1c1ea76b815ec1370756427e8db292c2900262
RAJ <rajkamal11126@gmail.com>
2020-08-21 00:10:10 +0530

    added email id

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 shopt -s extglob
-echo "Enter your Last name :" string
-read string
+echo "Enter your Email Id :"
+read email
 
-pat='^[[:upper:]]{1}[[:lower:]]{2,}$'
-if [[ $string =~ $pat ]]
+pat='^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$'
+if [[ $email =~ $pat ]]
 then
         echo "valid"
 else
